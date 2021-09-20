47d6f2e8ccd4fa14dff2361615df3c25ccf74756
Edson Jordan <edson_4555@hotmail.com>
2021-09-20 11:16:55 -0500

    adding options and icons into navLateral

--- routes/ticket.php
+++ routes/ticket.php
@@ -1,7 +1,6 @@
 <?php
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Tickets\HomeController;
 
-Route::get('/home', [HomeController::class, 'index']);
+//Route::get('/home', [HomeController::class, 'index'])->name("admin.ticket.index");
+Route::resource('/', HomeController::class)->names("admin.ticket");
